25e846b5aef2b2324e58f5402164abd12231a68d
donovanCarreno <donovan.carreno@gmail.com>
2016-06-22 21:49:08 -0700

    Implement many things

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,26 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
+    this.on('add', function() {
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    });
+
+    this.on('ended', function() {
+      this.remove(this.at(0));
+      if (this.at(0)) {
+        this.playFirst();
+      }
+    });
+
+    this.on('dequeue', function() {
+      this.remove(this.at(0)); // we might need to come back and take out element at an index, instead of first index
+    });
+  },
+
+  playFirst: function() {
+    this.at(0).play();
   }
 
 });
--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -1,6 +1,21 @@
 // Songs.js - Defines a backbone collection class for songs.
 var Songs = Backbone.Collection.extend({
 
-  model: SongModel
+  model: SongModel,
 
+  initialize: function() {
+    var context = this;
+    $.ajax({
+      type: 'GET',
+      url: this.url,
+      success: function(data) {
+        context.add(data.results);
+      },
+      error: function(data) {
+        console.log('error');
+      }
+    });
+  },
+
+  url: 'https://api.parse.com/1/classes/songs'
 });
